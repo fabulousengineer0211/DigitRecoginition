8cd276937ec37b55b031d5c6799f16c70fbff0d3
Vaibhav Marwaha <81986358+fabulousengineer0211@users.noreply.github.com>
2021-08-01 19:23:18 +0530

    Update and rename README.py to README

--- README.py
+++
@@ -1,1 +0,0 @@
-# Hi Everyone!!..This is starting of journey in Machine Learning. Here's my first project and I did everything, A to Z on google collab. Please have a look and feedbacks are always welcome!
